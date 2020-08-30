024e156d295428b447bde51c679151eb56bf813d
dykai <yingkaidang@gmail.com>
2020-08-30 22:10:14 +0800

    添加泛型

--- automata/sre.fs
+++ automata/sre.fs
@@ -2,12 +2,12 @@ module automata.sre
 
 open NFA
 
-type sre =
-    static member Is<'char when 'char: comparison>(c: 'char) =
-        NFA.ofSeq (Set [ 0; 1 ]) (Set [ c ]) 0 (Set [ 1 ]) [ (0, Char c, Set [ 1 ]) ]
-    static member Not<'char when 'char: comparison>(c: 'char) =
+type sre<'char when 'char: comparison> =
+    static member Is(c: 'char) = NFA.ofSeq (Set [ 0; 1 ]) (Set [ c ]) 0 (Set [ 1 ]) [ (0, Char c, Set [ 1 ]) ]
+    static member Not(c: 'char) =
         NFA.ofSeq (Set [ 0; 1 ]) (Set [ c ]) 0 (Set [ 1 ]) [ (0, NotIn(Set [ c ]), Set [ 1 ]) ]
-    static member In<'char when 'char: comparison>(cs: Set<'char>) =
+    static member In(cs: Set<'char>) =
         NFA.ofSeq (Set [ 0; 1 ]) cs 0 (Set [ 1 ]) (Set.map (fun c -> (0, Char c, Set [ 1 ])) cs)
-    static member NotIn<'char when 'char: comparison>(c: Set<'char>) =
+    static member NotIn(c: 'char seq) =
+        let c = c |> Set.ofSeq
         NFA.ofSeq (Set [ 0; 1 ]) c 0 (Set [ 1 ]) [ (0, NotIn c, Set [ 1 ]) ]
